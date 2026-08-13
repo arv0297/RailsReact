0c69d5a82f3f7c9953694087227323d9530a423a
alex-under <arv0297@gmail.com>
2026-08-12 23:18:12 -0400

    20260811225012 Fix brakeman update

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   end
   post "/graphql", to: "graphql#execute"
   root "home_pages#home"
-  resources :home_pages, only: [:index]
+  resources :home_pages, only: [ :index ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
